bbf4caa11f990ca430f5833f5d75dc12095d064f
nathanjessen <nathan.jessen@gmail.com>
2022-12-18 19:09:58 -0600

    disable broken addon-a11y

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,7 +4,7 @@ module.exports = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
-    '@storybook/addon-a11y',
+    // '@storybook/addon-a11y',
     'storybook-addon-theme-changer',
     'storybook-addon-designs',
     'addon-screen-reader',
